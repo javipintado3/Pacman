3b25aed82edac7ef3db63489c7d041a96d7c2838
Javier Pintado Navarro <javiertoncns03@gmail.com>
2024-09-02 13:20:09 +0200

    first commit

---
+++ assets/puntos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="puntos" tilewidth="4" tileheight="4" tilecount="1" columns="1">
+ <image source="puntos.png" width="4" height="4"/>
+</tileset>
---
+++ assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="tileset" tilewidth="32" tileheight="32" tilecount="99" columns="11">
+ <image source="tileset.png" width="358" height="295"/>
+</tileset>
